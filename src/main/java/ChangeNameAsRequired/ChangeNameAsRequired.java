5a35f4577ae033e0a10ce372a87d4495e75bedb7
Saad01298 <mdsaadali01298@gmail.com>
2022-07-11 15:03:16 +0530

    first

---
+++ src/main/java/ChangeNameAsRequired/ChangeNameAsRequired.java
@@ -0,0 +1,13 @@
+package ChangeNameAsRequired;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangeNameAsRequired {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChangeNameAsRequired.class, args);
+	}
+
+}
---
+++ src/test/java/ChangeNameAsRequired/ChangeNameAsRequired.java
@@ -0,0 +1,13 @@
+package ChangeNameAsRequired;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChangeNameAsRequired {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
